3f0031ff7e3bccfedea17840d6f5050699bf6dbb
alvin <alvinhtml@gmail.com>
2019-08-23 15:17:38 +0800

    Added CircleCi build configuration file

---
+++ .circleci/deploy-master.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+user=root
+host=$1
+port=$2
+
+src=`pwd`"/dist/"
+des=/home/wwwroot/note/
+now=`date +"%Y-%m-%d %H:%M:%S"`
+
+echo "将 $src 目录下的文件同步到 $host:$des 目录下"
+rsync -zrc -e "ssh -p $port" --exclude '*.map' $src $user@$host:$des
+echo "$now update $host $des code"
