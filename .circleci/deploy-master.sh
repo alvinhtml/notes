a44fdac8101e22ddef53aae2d232d384215136a3
Alvin <alvinhtml@gmail.com>
2021-11-18 17:46:01 +0800

    update deploy script

--- .circleci/deploy-master.sh
+++ .circleci/deploy-master.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-user=root
+user=ec2-user
 host=$1
 port=$2
 
@@ -9,5 +9,6 @@ des=/home/wwwroot/note/
 now=`date +"%Y-%m-%d %H:%M:%S"`
 
 echo "将 $src 目录下的文件同步到 $host:$des 目录下"
+rsync -
 rsync -zrc -e "ssh -p $port" --exclude '*.map' $src $user@$host:$des
 echo "$now update $host $des code"
